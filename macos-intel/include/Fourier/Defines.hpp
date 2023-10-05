670dc38096fd8d88502031a9a4040cc68c509964
Chuvak <demenev2002@yandex.ru>
2023-10-05 23:33:00 +0300

    Change format of repository

--- macos-intel/include/Fourier/Defines.hpp
+++
@@ -1,7 +0,0 @@
-
-#ifndef _FOURIER_DEFINES_HPP_
-#define _FOURIER_DEFINES_HPP_
-
-#include <SFML/Config.hpp>
-
-#endif
--- macos-intel/include/Fourier/View.hpp
+++
@@ -1,7 +0,0 @@
-
-#ifndef _VIEW_HPP_
-#define _VIEW_HPP_
-
-void init_view(void);
-
-#endif
